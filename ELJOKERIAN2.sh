991b5aac494af07fca7e8539e3df53aa99eba5a2
CAPOELJOKERIAN <57456030+CAPOELJOKERIAN@users.noreply.github.com>
2019-11-07 23:35:26 +0200

    Add files via upload

---
+++ ELJOKERIAN2.sh
@@ -0,0 +1,243 @@
+#!/bin/sh
+#colors
+dir=/data/data/com.termux/files/home
+
+
+clear
+cmatrix -C red -s
+red='\e[1;31m'
+green='\e[1;32m'
+blue='\e[1;34m'
+purple='\e[1;35m'
+cyan='\e[1;36m'
+white='\e[1;37m'
+yellow='\e[1;33m'
+echo " CCCC      A     PPPPPPP     OOO"
+echo "CC        A A    P      P  O     O"
+echo "CC       A   A   PPPPPPP  O   0   O"
+echo "CC      AAAAAAA  P         O     O"
+echo " CCCC  A       A P           OOO"
+echo""
+echo  '(1)-->Lazymux'
+echo  '(2)-->sqlmap'
+echo  '(3)-->RED_HAWK'
+echo  '(4)-->hasher'
+echo  '(5)-->shellphish'
+echo  ''
+echo  '(6)-->fb-group-hack'
+echo  '(7)-->blackhatislamicddos'
+echo  '(8)-->EasY_HaCk'
+echo  '(9)-->Tmux-Bunch'
+echo  '(10)-->Spammer-Grab'
+echo  ''
+echo  '(11)-->Report'
+echo  '(12)-->xerxes'
+echo  '(13)-->instagram-tools'
+echo  '(14)-->TheFatRat'
+echo  '(15)-->Scarshot'
+echo  ''
+echo  '(16)-->Exploitinsta'
+echo  '(17)-->Dzjecter'
+echo  '(18)-->riskiyans'
+echo  '(19)-->ELJOKERIAN'
+echo  '(20)-->SETAN'
+echo  ''
+echo  '(21)-->instainsane'
+echo  '(22)-->lizard'
+echo  '(23)-->spam cs'
+echo  '(24)-->A-Rat'
+echo''
+echo '  XxXxXxX'
+echo''
+echo -n  "-----*capo*----" ; read a ; echo ''
+
+if [ $a -eq 1 -r 01 ]; then
+
+clear
+figlet -f big 'Lazymux'
+git clone https://github.com/Gameye98/Lazymux.git
+figlet -f big 'finished'
+
+
+fi
+if [ $a -eq 2 -r 01 ]; then
+
+clear
+figlet -f big 'sqlmap'
+git clone https://github.com/sqlmapproject/sqlmap
+figlet -f big 'finished'
+
+fi
+if [ $a -eq 3 ];then
+
+clear
+figlet -f big 'RED_HAWK'
+git clone https://github.com/Tuhinshubhra/RED_HAWK
+figlet -f big 'finished'
+
+fi
+if [ $a -eq 4 ];then
+
+clear
+figlet -f big 'hasher'
+git clone https://github.com/ciku370/hasher
+figlet -f big 'finished'
+
+fi
+if [ $a -eq 5 ];then
+
+clear
+figlet -f big 'shellphish'
+git clone https://github.com/thelinuxchoice/shellphish
+figlet -f big 'finished'
+
+fi
+if [ $a -eq 6 ];then
+
+clear
+figlet -f big 'fb-group-hack'
+git clone https://github.com/Mr-NemrMedo/fb-group-hack
+figlet -f big 'finished'
+
+fi
+if [ $a -eq 7 ] ;then
+
+clear
+figlet -f big 'blackhatislamicddos'
+git clone https://github.com/DatabaseHk/blackhatislamicddos
+figlet -f big 'finished'
+
+fi
+if [ $a -eq 8 ] ;then
+
+clear
+figlet -f big 'EasY_HaCk'
+git clone https://github.com/sabri-zaki/EasY_HaCk
+figlet -f big 'finished'
+fi
+if [ $a -eq 9 ] ;then
+
+clear
+figlet -f big 'Tmux-Bunch'
+git clone https://github.com/Hax4us/Tmux-Bunch
+figlet -f big 'finished'
+fi
+if [ $a -eq 10 ] ;then
+
+clea
+figlet f big 'Spammer-Grab'
+git clone https://github.com/Noxturnix/Spammer-Grab
+figlet -f big 'finished'
+
+fi
+if [ $a -eq 11 ] ;then
+
+clear
+figlet -f big 'Report'
+git clone https://github.com/IlayTamvan/Report
+figlet -f big 'finished'
+
+fi
+if [ $a -eq 12 ] ;then
+
+clear
+figlet -f big 'xerxes'
+git clone https://github.com/zanyarjamal/xerxes
+figlet -f big 'finished'
+
+fi
+if [ $a -eq 13 ] ;then
+
+clear
+figlet -f big 'instagram-tools'
+git clone https://github.com/masokky/instagram-tools.git
+figlet -f big 'finished'
+
+fi
+if [ $a -eq 14 ] ;then
+
+clear
+figlet -f big 'TheFatRat'
+git clone https://github.com/Screetsec/TheFatRat.git
+figlet -f big 'finished'
+fi
+if [ $a -eq 15 ] ;then
+
+clear
+figlet -f big 'Scarshot'
+git clone https://github.com/ahmedtamer65/Scarshot
+figlet -f big 'finished'
+
+fi
+if [ $a -eq 16  ] ;then
+
+clear
+figlet -f big 'Exploitinsta'
+git clone https://github.com/Nikhiltripathi90/Exploitinsta
+figlet -f big 'finished'
+
+fi
+if [ $a -eq 17  ] ;then
+
+clear
+figlet -f big 'Dzjecter'
+git clone https://github.com/404system/Dzjecter
+figlet -f big 'finished'
+
+fi
+if [ $a -eq 18 ] ;then
+
+clear
+figlet -f big 'riskiyans'
+git clone https://github.com/Riskiyans/riskiyans
+figlet -f big 'finished'
+
+fi
+if [ $a -eq 19 ] ;then
+
+clear
+figlet -f big 'ELJOKERIAN'
+git clone https://github.com/CAPOELJOKERIAN/ELJOKERIAN.git
+figlet -f big 'finished'
+
+fi
+if [ $a -eq 20 ] ;then
+
+clear
+figlet -f big 'SETAN'
+git clone https://github.com/pashayogi/SETAN
+figlet -f big 'finished'
+
+fi
+if [ $a -eq 21 ] ;then
+
+clear
+figlet -f big 'instainsane'
+https://github.com/thelinuxchoice/instainsane
+figlet -f big 'finished'
+
+fi
+if [ $a -eq 22 ] ;then
+
+clear
+figlet -f big 'lizard'
+git clone https://github.com/thelinuxchoice/lizard
+figlet -f big 'finished'
+
+fi
+if [ $a -eq 23 ] ;then
+
+clear
+figlet -f big 'spam cs'
+git clone https://github.com/CrazyError/CS
+figlet -f big 'finished'
+
+fi
+if [ $a -eq 24 ] ;then
+
+clear
+figlet -f big 'A-Rat'
+git clonehttps://github.com/Xi4u7/A-Rat
+figlet -f big 'finished'
+
+fi
